1ae9d1edb71b1e9fcf5033c156b1556f56948b0c
dougllas.sousa <dougllas.sousa@seduc.ce.gov.br>
2017-10-03 08:43:08 -0300

    Initial Commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+https://docs.spring.io/spring-boot/docs/current/reference/html/common-application-properties.html
+
+Modelo de Projeto para gerar aplicacao spring boot configurada com database
---
+++ src/main/java/org/dougllas/springboot/configuration/BeforeStart.java
@@ -0,0 +1,12 @@
+package org.dougllas.springboot.configuration;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BeforeStart implements CommandLineRunner{
+
+    public void run(String... strings) throws Exception {
+
+    }
+}
